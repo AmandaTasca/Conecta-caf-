8507ab6e20994d6b89d9a32954b4a2ca2173cec9
AmandaTasca <amandatasca012@gmail.com>
2024-04-22 16:44:58 -0300

    Alterações Salvas

--- ConectaCafe/Models/Avaliacao.cs
+++ ConectaCafe/Models/Avaliacao.cs
@@ -15,12 +15,7 @@ public class Avaliacao
      [StringLength(60, ErrorMessage = " O nome deve possuir no máximo 60 caracteres")]
     public string Pessoa { get; set; }
         
-     [Display( Name = "Título")]
-     [Required(ErrorMessage = "Por favor, Informe o titulo. ")]
-     [StringLength(100, ErrorMessage = "O titulo deve possuir no máximo 100 caracteres")]
-    public string Titulo { get; set; }
-    
-  
+
     [Required(ErrorMessage = "Por favor, Informe o texto. ")]
      [StringLength(500, ErrorMessage = "O texto deve possuir no máximo 500 caracteres")]
     public string Texto { get; set; }
@@ -28,9 +23,14 @@ public class Avaliacao
     
     [Column(TypeName = "decimal(1,0)")]
     [Required(ErrorMessage = "Por favor, Informe o preço. ")]
-    public decimal Nota { get; set; }
+    public decimal Nota { get; set; } = 5;
+
 
     [Display( Name ="Data da Avaliação")]
     [DataType(DataType.Date)]
-    public DateTime DataAvaliacao { get; set; }
+    public DateTime DataAvaliacao { get; set; } = DateTime.Now;
+
+
+     [StringLength(200)]
+    public string Foto { get; set; }
 }
